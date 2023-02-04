5164e20e9211d7f0588bca7c1b756dcbde10355e
Nikolai <107433192+NikolasSh@users.noreply.github.com>
2023-02-04 23:34:13 +0300

    Add files home work

---
+++ Task8.cs
@@ -0,0 +1,50 @@
+﻿/*
+Напишите программу, которая принимает на вход пятизначное число
+ и проверяет, является ли оно палиндромом.
+*/
+
+int GetNumber(string message)
+{
+    int result = 0;
+
+    while(true)
+    {
+        Console.WriteLine(message);
+        
+        if(int.TryParse(Console.ReadLine(), out result) && result < 99999 && result > 9999)
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Ввели не верное число или 0. Повторите ввод!");
+        }
+    }
+
+    return result;
+}
+
+int number = GetNumber("Введите пятизначное число: ");
+int a = number % 10;
+Console.WriteLine(a);
+int a1 = number / 10000;
+Console.WriteLine(a1);
+int b = number / 10;
+Console.WriteLine(b);
+int b1 = b % 10;
+Console.WriteLine(b1);
+int c = number / 1000;
+Console.WriteLine(c);
+int c1 = c % 10;
+Console.WriteLine(c1);
+
+
+
+if (a == a1 && b1 == c1)
+{
+    Console.WriteLine($"Число {number} является палиндромом");
+}
+else 
+{
+    Console.WriteLine($"Число {number} не является палиндромом");
+}
---
+++ Task9.cs
@@ -0,0 +1,45 @@
+﻿/*
+Напишите программу, которая принимает на вход 
+координаты двух точек и находит расстояние между ними в 3D пространстве.
+*/
+/*
+Корень((x1-x2)^2 + (y1-y2)^2 + (z1-z2)^2)
+Math.Sqrt()
+Math.Pow(выражение, степень)
+*/
+
+int GetNumber(string message)
+{
+    int result = 0;
+
+    while(true)
+    {
+        Console.WriteLine(message);
+        
+        if(int.TryParse(Console.ReadLine(), out result))
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Ввели не число или 0. Повторите ввод");
+        }
+    }
+
+    return result;
+}
+
+int x1 = GetNumber("Введите координату Х первой точки");
+int y1 = GetNumber("Введите координату Y первой точки");
+int z1 = GetNumber("Введите координату Z первой точки");
+int x2 = GetNumber("Введите координату Х второй точки");
+int y2 = GetNumber("Введите координату Y второй точки");
+int Z2 = GetNumber("Введите координату Z второй точки");
+
+double GetDistance (int x1, int x2, int y1, int y2, int z1, int z2)
+{
+    double result = Math.Sqrt(Math.Pow(x1-x2,2) + Math.Pow(y1-y2,2) + Math.Pow(z1-z2,2));
+    return result;
+} 
+
+Console.WriteLine(GetDistance(x1,x2,y1,y2,z1,Z2));
